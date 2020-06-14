4b1197036616021a1e3f740fd4c76fb491e8804b
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-06-13 21:38:14 -0400

    not as jumpy

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,100 +1,373 @@
 import React, { Component } from "react";
+import $ from "jquery";
+
+function button() {
+  $(function () {
+    // Vars
+    var pointsA = [],
+      pointsB = [],
+      $canvas = null,
+      canvas = null,
+      context = null,
+      points = 8,
+      viscosity = 20,
+      mouseDist = 70,
+      damping = 0.05,
+      showIndicators = false;
+    var mouseX = 0;
+    var mouseY = 0;
+    var relMouseX = 0;
+    var relMouseY = 0;
+    var mouseLastX = 0;
+    var mouseLastY = 0;
+    var mouseDirectionX = 0;
+    var mouseDirectionY = 0;
+    var mouseSpeedX = 0;
+    var mouseSpeedY = 0;
+
+    /**
+     * Get mouse direction
+     */
+    function mouseDirection(e) {
+      if (mouseX < e.pageX) mouseDirectionX = 1;
+      else if (mouseX > e.pageX) mouseDirectionX = -1;
+      else mouseDirectionX = 0;
+
+      if (mouseY < e.pageY) mouseDirectionY = 1;
+      else if (mouseY > e.pageY) mouseDirectionY = -1;
+      else mouseDirectionY = 0;
+
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+
+      relMouseX = mouseX - $canvas.offset().left;
+      relMouseY = mouseY - $canvas.offset().top;
+    }
+    $(document).on("mousemove", mouseDirection);
+
+    /**
+     * Get mouse speed
+     */
+    function mouseSpeed() {
+      mouseSpeedX = mouseX - mouseLastX;
+      mouseSpeedY = mouseY - mouseLastY;
+
+      mouseLastX = mouseX;
+      mouseLastY = mouseY;
+
+      setTimeout(mouseSpeed, 50);
+    }
+    mouseSpeed();
+
+    /**
+     * Init button
+     */
+    function initButton() {
+      // Get button
+      var button = $(".btn-liquid");
+      var buttonWidth = button.width();
+      var buttonHeight = button.height();
+
+      // Create canvas
+      $canvas = $("<canvas></canvas>");
+      button.append($canvas);
+
+      canvas = $canvas.get(0);
+      canvas.width = buttonWidth + 100;
+      canvas.height = buttonHeight + 100;
+      context = canvas.getContext("2d");
+
+      // Add points
+
+      var x = buttonHeight / 2;
+      for (var j = 1; j < points; j++) {
+        addPoints(x + ((buttonWidth - buttonHeight) / points) * j, 0);
+      }
+      addPoints(buttonWidth - buttonHeight / 5, 0);
+      addPoints(buttonWidth + buttonHeight / 10, buttonHeight / 2);
+      addPoints(buttonWidth - buttonHeight / 5, buttonHeight);
+      for (j = points - 1; j > 0; j--) {
+        addPoints(
+          x + ((buttonWidth - buttonHeight) / points) * j,
+          buttonHeight
+        );
+      }
+      addPoints(buttonHeight / 5, buttonHeight);
+
+      addPoints(-buttonHeight / 10, buttonHeight / 2);
+      addPoints(buttonHeight / 5, 0);
+
+      renderCanvas();
+    }
+
+    /**
+     * Add points
+     */
+    function addPoints(x, y) {
+      pointsA.push(new Point(x, y, 1));
+      pointsB.push(new Point(x, y, 2));
+    }
+
+    /**
+     * Point
+     */
+    function Point(x, y, level) {
+      this.x = this.ix = 50 + x;
+      this.y = this.iy = 50 + y;
+      this.vx = 0;
+      this.vy = 0;
+      this.cx1 = 0;
+      this.cy1 = 0;
+      this.cx2 = 0;
+      this.cy2 = 0;
+      this.level = level;
+    }
+
+    Point.prototype.move = function () {
+      this.vx += (this.ix - this.x) / (viscosity * this.level);
+      this.vy += (this.iy - this.y) / (viscosity * this.level);
+
+      var dx = this.ix - relMouseX,
+        dy = this.iy - relMouseY;
+      var relDist = 1 - Math.sqrt(dx * dx + dy * dy) / mouseDist;
+
+      // Move x
+      if (
+        (mouseDirectionX > 0 && relMouseX > this.x) ||
+        (mouseDirectionX < 0 && relMouseX < this.x)
+      ) {
+        if (relDist > 0 && relDist < 1) {
+          this.vx = (mouseSpeedX / 4) * relDist;
+        }
+      }
+      this.vx *= 1 - damping;
+      this.x += this.vx;
+
+      // Move y
+      if (
+        (mouseDirectionY > 0 && relMouseY > this.y) ||
+        (mouseDirectionY < 0 && relMouseY < this.y)
+      ) {
+        if (relDist > 0 && relDist < 1) {
+          this.vy = (mouseSpeedY / 4) * relDist;
+        }
+      }
+      this.vy *= 1 - damping;
+      this.y += this.vy;
+    };
+
+    /**
+     * Render canvas
+     */
+    function renderCanvas() {
+      var rafID = requestAnimationFrame(renderCanvas);
+
+      // Clear scene
+      context.clearRect(0, 0, $canvas.width(), $canvas.height());
+      context.fillStyle = "rgba(33, 116, 135, 0)";
+
+      context.fillRect(0, 0, $canvas.width(), $canvas.height());
+
+      // Move points
+      for (var i = 0; i <= pointsA.length - 1; i++) {
+        pointsA[i].move();
+        pointsB[i].move();
+      }
+
+      // Create dynamic gradient
+      var gradientX = Math.min(
+        Math.max(mouseX - $canvas.offset().left, 0),
+        $canvas.width()
+      );
+      var gradientY = Math.min(
+        Math.max(mouseY - $canvas.offset().top, 0),
+        $canvas.height()
+      );
+      var distance =
+        Math.sqrt(
+          Math.pow(gradientX - $canvas.width() / 2, 2) +
+            Math.pow(gradientY - $canvas.height() / 2, 2)
+        ) /
+        Math.sqrt(
+          Math.pow($canvas.width() / 2, 2) + Math.pow($canvas.height() / 2, 2)
+        );
+
+      var gradient = context.createRadialGradient(
+        gradientX,
+        gradientY,
+        300 + 300 * distance,
+        gradientX,
+        gradientY,
+        0
+      );
+
+      gradient.addColorStop(0, "#88B161");
+      gradient.addColorStop(1, "#8884FF");
+
+      // Draw shapes
+      var groups = [pointsA, pointsB];
+
+      for (var j = 0; j <= 1; j++) {
+        var points = groups[j];
+
+        if (j === 0) {
+          // Background style
+
+          context.fillStyle = "#1CE2D8";
+        } else {
+          // Foreground style
+          context.fillStyle = gradient;
+        }
+
+        context.beginPath();
+        context.moveTo(points[0].x, points[0].y);
+
+        for (i = 0; i < points.length; i++) {
+          var p = points[i];
+          var nextP = points[i + 1];
+
+          if (nextP !== undefined) {
+            p.cx1 = (p.x + nextP.x) / 2;
+            p.cy1 = (p.y + nextP.y) / 2;
+            p.cx2 = (p.x + nextP.x) / 2;
+            p.cy2 = (p.y + nextP.y) / 2;
+
+            context.bezierCurveTo(p.x, p.y, p.cx1, p.cy1, p.cx1, p.cy1);
+            // 	continue;
+            // }
+
+            // context.bezierCurveTo(p.cx1, p.cy1, p.cx2, p.cy2, nextP.x, nextP.y);
+          } else {
+            nextP = points[0];
+            p.cx1 = (p.x + nextP.x) / 2;
+            p.cy1 = (p.y + nextP.y) / 2;
+
+            context.bezierCurveTo(p.x, p.y, p.cx1, p.cy1, p.cx1, p.cy1);
+          }
+        }
+
+        // context.closePath();
+        context.fill();
+      }
+
+      if (showIndicators) {
+        // Draw points
+        context.fillStyle = "#000";
+        context.beginPath();
+        for (i = 0; i < pointsA.length; i++) {
+          p = pointsA[i];
+
+          context.rect(p.x - 1, p.y - 1, 2, 2);
+        }
+        context.fill();
+
+        // Draw controls
+        context.fillStyle = "#f00";
+        context.beginPath();
+        for (i = 0; i < pointsA.length; i++) {
+          p = pointsA[i];
+
+          context.rect(p.cx1 - 1, p.cy1 - 1, 2, 2);
+          context.rect(p.cx2 - 1, p.cy2 - 1, 2, 2);
+        }
+        context.fill();
+      }
+    }
+
+    // Init
+    initButton();
+  });
+}
 
 export default class Section1 extends Component {
-                 state = {
-                   what: 2,
-                   winScroll: 0,
-                   oppositeOpacity:100,
-                   fontSize: 60,
-                   fontSize2: 30,
-                   moveLeft: 0,
-                   moveTop:0
-                 };
+  state = {
+    what: 2,
+    winScroll: 0,
+    oppositeOpacity: 100,
+    oppositeOpacity2: 100,
+    fontSize: 60,
+    fontSize2: 30,
+    moveLeft: 0,
+    moveTop: 0,
+  };
 
-                 componentDidMount() {
-                   window.addEventListener("scroll", this.listenToScroll);
-                 }
-                
-                 listenToScroll = () => {
-                   const winScroll = document.documentElement.scrollTop>1000?
-                     document.body.scrollTop+window.innerHeight:
-                     document.documentElement.scrollTop;
-                     const adjust = winScroll/8; 
-                     const opacity = 140 - adjust*2; 
-                     const size = 60 - adjust;
-                     const size2 = 30 - (.5*adjust); 
-                     const moveLeft = winScroll;
-                     const moveTop = winScroll/(1.5)
-                      
-                     console.log("this is the height" + winScroll);
+  componentDidMount() {
+    window.addEventListener("scroll", this.listenToScroll);
+    button();
+  }
 
-                     this.setState({winScroll:winScroll})
-                     this.setState({oppositeOpacity:opacity})
-                     this.setState({fontSize:size})
-                     this.setState({fontSize2:size2})
-                     this.setState({moveLeft:moveLeft})
-                     this.setState({moveTop:moveTop})
-                 };
+  listenToScroll = () => {
+    const winScroll =
+      document.documentElement.scrollTop > 1000
+        ? document.body.scrollTop + window.innerHeight
+        : document.documentElement.scrollTop;
+    const adjust = winScroll / 8;
+    const opacity = 140 - adjust * 2;
+    const opacity2 = 140 - adjust * 8;
+    const size = 60 - adjust;
+    const size2 = 30 - 0.5 * adjust;
+    const moveLeft = winScroll;
+    const moveTop = winScroll / 1.5;
 
-                 render() {
+    console.log("this is the height" + winScroll);
 
-                   return (
-                     <div>
-                       console.log({"HELLO"})
-                       <section
-                         id="hero1"
-                         className="hero"
-                         loop
-                         autoPlay
-                         playsInline
-                       >
-                         <div className="inner">
-                           <div className="copy">
-                             <div
-                               style={{
-                                 opacity: this.state.oppositeOpacity + "%",
-                                 marginTop: this.state.moveTop + "px",
-                               }}
-                               id="display"
-                             >
-                               <p
-                                 style={{
-                                   fontSize: this.state.fontSize + "px",
-                                 }}
-                                 id="name"
-                               >
-                                 Matthew Krebs
-                               </p>
-                               <p></p>
-                               <p
-                                 style={{
-                                   fontSize: this.state.fontSize2 + "px",
-                                 }}
-                                 id="title"
-                               >
-                                 Full Stack Web Developer
-                               </p>
-                               <br />
-                               <div
-                                 style={{
-                                   marginLeft: this.state.moveLeft + "px",
-                                   marginTop: this.state.moveLeft + "px",
-                                 }}
-                                 id="newButton"
-                               >
-                                 <a href="#myskills" className="btn-liquid">
-                                   <span className="extra">
-                                     Click to see my Skills
-                                   </span>
-                                 </a>
-                               </div>
-                               <div></div>
-                             </div>
-                           </div>
-                         </div>
-                       </section>
-                     </div>
-                   );
-                 }
-               
-               }
+    this.setState({ winScroll: winScroll });
+    this.setState({ oppositeOpacity: opacity });
+    this.setState({ oppositeOpacity2: opacity2 });
+    this.setState({ fontSize: size });
+    this.setState({ fontSize2: size2 });
+    this.setState({ moveLeft: moveLeft });
+    this.setState({ moveTop: moveTop });
+  };
+
+  render() {
+    return (
+      <div>
+        console.log({"HELLO"})
+        <section id="hero1" className="hero" loop autoPlay playsInline>
+          <div className="inner">
+            <div className="copy">
+              <div
+                style={{
+                  opacity: this.state.oppositeOpacity2 + "%",
+                  marginTop: this.state.moveTop + "px",
+                }}
+                id="display"
+              >
+                <p
+                  style={{
+                    fontSize: this.state.fontSize + "px",
+                  }}
+                  id="name"
+                >
+                  Matthew Krebs
+                </p>
+                <p></p>
+                <p
+                  style={{
+                    fontSize: this.state.fontSize2 + "px",
+                  }}
+                  id="title"
+                >
+                  Full Stack Web Developer
+                </p>
+                <br />
+                <div
+                  style={{
+                    opacity: this.state.oppositeOpacity2 + "%",
+                  }}
+                  id="newButton"
+                >
+                  <a href="#myskills" className="btn-liquid">
+                    <span className="extra">Click to see my Skills</span>
+                  </a>
+                </div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}

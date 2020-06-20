3e72088bebd954212ed6ad33338b56442e86e10f
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-06-20 11:12:29 -0400

    fix css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -25,25 +25,23 @@ e.target.style.overflow = "hidden";
         >
           <figure
             id={this.props.id}
-            className={this.props.highlight ? "red" : ""}
+            className={this.props.highlight ? "red tint" : "tint"}
           >
-            <div className = "tint">
-              <img
-                className="photos"
-                onMouseOver={changeBackground}
-                onClick={this.props.showRelated.bind(this, nameofid)}
-                data-toggle="tooltip"
-                data-html="true"
-                title={this.props.title}
-                src={this.props.image}
-                alt={this.props.alt}
-                style={{
-                  width: "inherit",
-                  height: "inherit",
-                  overflow: "hidden",
-                }}
-              />
-            </div>
+            <img
+              className="photos"
+              onMouseOver={changeBackground}
+              onClick={this.props.showRelated.bind(this, nameofid)}
+              data-toggle="tooltip"
+              data-html="true"
+              title={this.props.title}
+              src={this.props.image}
+              alt={this.props.alt}
+              style={{
+                width: "inherit",
+                height: "inherit",
+                overflow: "hidden",
+              }}
+            />
           </figure>
         </Link>
       </div>
